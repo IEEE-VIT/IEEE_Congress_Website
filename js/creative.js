6bba1a6d893ce33bbbe06c0f205a1e57bf721a41
EkaanshArora <blue.name68@gmail.com>
2018-02-25 06:47:41 +0530

    Code Clean-up, fixed issue #17

--- js/creative.js
+++ js/creative.js
@@ -39,23 +39,6 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-  // Scroll reveal calls
-  window.sr = ScrollReveal();
-  sr.reveal('.sr-icons', {
-    duration: 600,
-    scale: 0.3,
-    distance: '0px'
-  }, 200);
-  sr.reveal('.sr-button', {
-    duration: 1000,
-    delay: 200
-  });
-  sr.reveal('.sr-contact', {
-    duration: 600,
-    scale: 0.3,
-    distance: '0px'
-  }, 300);
-
   // Magnific popup calls
   $('.popup-gallery').magnificPopup({
     delegate: 'a',
